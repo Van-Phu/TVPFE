20b6d9325250cf1ef9683f6798a4019c4e2e943d
Kaeyn <94393201+Kaeyn@users.noreply.github.com>
2024-11-25 21:51:23 +0700

    Revert "Merge branch 'phu.luong.dev' into tam.truong"
    This reverts commit 16560222774eb447b8a1757b9e83d2dea223efd9, reversing
    changes made to 82034ddae9f37f76d73586e247512fad64868298.

--- dev/src/App.js
+++ dev/src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import {AppRoutes, Navbar} from '../src/components/components'
+import AppRoutes from './layout/AppRoutes';
+import { Navbar } from './layout/shared/component/C-NavBar/navbar';
+import './App.css'
+import { CategoryList } from './layout/shared/component/C-CategoryList/categorylist';
 
 function App() {
   return (
-    <div>
-      <AppRoutes/>
-    </div>
+    <div className='container-app'>
+      <Router>
+        <div className='layout-app'>
+          <div className='navbar-area'>
+            <Navbar />
+          </div>
 
-    // <div className='container-app'>
-    //     <div className='layout-app'>
-    //       <div className='navbar-area'>
-    //         <Navbar />
-    //       </div>
-
-    //       <div className='rightContent'>
-    //         <div className='titleApp'>
-    //             Hello world
-    //         </div>
-    //         <div className='route-area'>
-    //           <AppRoutes />
-    //         </div>
+          <div className='rightContent'>
+            <div className='titleApp'>
+                Hello world
+            </div>
+            <div className='route-area'>
+              <AppRoutes />
+            </div>
             
-    //       </div>
+          </div>
           
-    //     </div>
-
-    // </div>
+        </div>
+      </Router>
+    </div>
 
   );
 }
